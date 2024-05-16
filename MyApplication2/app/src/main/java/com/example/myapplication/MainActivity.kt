f90c66a68334ce78684fc1ecedd5d9d26c8bb249
mahdis-fathi <88924157+mahdis-fathi@users.noreply.github.com>
2024-05-17 00:34:54 +0430

    phase 1 completed
    با استفاده از سرویس و برودکست در شروع برنامه نوتیفیکیشن میزان باتری برنامه ارسال میشود و همچنین با تغییرات اتصال اینترنت نوتیفکیشن مناسب آن نیز ارسال میشود.

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.os.BatteryManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -22,10 +26,11 @@ import java.util.concurrent.TimeUnit
 @Suppress("PreviewNotSupportedInUnitTestFiles")
 class MainActivity : ComponentActivity() {
 
+    val connectivityReceiver = ConnectivityReceiver()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        registerReceiver(connectivityReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -38,9 +43,35 @@ class MainActivity : ComponentActivity() {
             }
         }
 
+        val batteryLevel = getBatteryLevel()
+        sendBatteryNotification(batteryLevel)
+
 
     }
 
+    private fun getBatteryLevel(): Int {
+        val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+            applicationContext.registerReceiver(null, ifilter)
+        }
+        val level: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        val scale: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+        return (level * 100 / scale.toFloat()).toInt()
+    }
+
+    private fun sendBatteryNotification(batteryLevel: Int) {
+        val channelId = CounterNotificartionService.COUNTER_CHANNEL_ID
+        val notificationId = 1
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.baseline_battery_unknown_24)
+            .setContentTitle("Battery Level")
+            .setContentText("Current battery level: $batteryLevel%")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
     @Composable
     fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(
@@ -59,6 +90,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        //unregisterReceiver(airplaneModeReceive)
+        unregisterReceiver(connectivityReceiver)
     }
 }
